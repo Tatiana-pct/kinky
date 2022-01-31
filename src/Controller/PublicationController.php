23e1ff093cb8d5b4920db2a8f567a2f1c648aadc
Tatiana-pct <79706218+Tatiana-pct@users.noreply.github.com>
2022-01-31 14:58:34 +0100

    Création du PublicationController.php + vues

--- src/Controller/PublicationController.php
+++ src/Controller/PublicationController.php
@@ -6,15 +6,54 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/publications", name="publication_")
+ */
 class PublicationController extends AbstractController
 {
     /**
-     * @Route("/publication", name="publication")
+     * @Route("", name="list")
      */
-    public function index(): Response
+    public function list(): Response
     {
-        return $this->render('publication/index.html.twig', [
-            'controller_name' => 'PublicationController',
+        //todo: aller chercher les publications en BDD
+        return $this->render('publication/list.html.twig', [
+
         ]);
     }
+
+    /**
+     * @Route("/details/{id}", name="details")
+     */
+    public function details(int $id): Response
+    {
+
+        //todo: aller chercher la serie dans la BDD
+        return $this->render('publication/details.html.twig', [
+
+        ]);
+    }
+
+    /**
+     * @Route("/create", name="create")
+     */
+    public function create(): Response
+    {
+        return $this->render('publication/create.html.twig', [
+
+        ]);
+    }
+
+
+    ///  **
+    //   * @Route("/publications", name="publication_delete")
+    //   */
+    //   public function delete(): Response
+    //   {
+    //    return $this->render('publication/delete.html.twig', [
+    //       ]);
+    //    }
+
+
+
 }
